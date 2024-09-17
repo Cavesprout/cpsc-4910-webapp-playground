ba2f73e3f9ec1dc3a5c3fef3273c308f1ef62a29
Scott Logan <thecaveisstilldark@gmail.com>
2024-09-16 23:00:43 -0400

    Functional login/signup system

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import { fetchAuthSession } from 'aws-amplify/auth';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
+import { cognitoUserPoolsTokenProvider } from 'aws-amplify/auth/cognito';
+import { CookieStorage } from 'aws-amplify/utils';
+
+cognitoUserPoolsTokenProvider.setKeyValueStorage(new CookieStorage());
+
+Amplify.configure({ ...awsExports });
+let token = (await fetchAuthSession()).tokens?.idToken?.toString();
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Authenticator signUpAttributes={["birthdate", "name"]}>
+        {({ signOut, user }) => (
+            <div>
+              <p>Welcome {user.username}</p>
+                <p>Your token is: {token}</p>
+              <button onClick={signOut}>Sign out</button>
+            </div>
+        )}
+      </Authenticator>
   );
 }
+
 
 export default App;
