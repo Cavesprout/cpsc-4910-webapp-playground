4af34da1d190938a32190aae54d0b97e12357ba8
Scott Logan <thecaveisstilldark@gmail.com>
2024-09-21 19:17:55 -0400

    Added test login app for next.js

---
+++ login-app-next/src/app/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React from 'react';
+import { Amplify } from 'aws-amplify';
+
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from '../aws-exports';
+
+Amplify.configure({ ...awsExports });
+
+
+export default function Home() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
